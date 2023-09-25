db24dc3a63efd1f90da4ca1aa9ee41d0d06f46f5
flash11 <vlad.dalevich@bk.ru>
2023-09-25 21:44:07 +0700

    add tests

---
+++ Task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTest {
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void heapify() {
+    }
+
+    @Test
+    void printArray() {
+    }
+
+    @Test
+    void main() {
+    }
+}
