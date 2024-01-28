9bb12def112286a1837ef8b7a151800bef62f5c7
Vladislav Dalevich <v.dalevich@g.nsu.ru>
2024-01-29 01:31:04 +0700

    update
    Signed-off-by: Vladislav Dalevich <v.dalevich@g.nsu.ru>

--- Task_1_1_1/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -1,24 +1,59 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import java.util.Arrays;
+import java.util.Random;
+import org.example.HeapSort;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class HeapSortTest {
+public class HeapSortTest {
 
     @Test
-    void sort() {
+    public void testHeapSort() {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        int[] expected = {5, 6, 7, 11, 12, 13};
+
+        HeapSort.sort(arr);
+
+        assertArrayEquals(expected, arr);
     }
 
     @Test
-    void heapify() {
+    public void testEmptyArray() {
+        int[] arr = {};
+        int[] expected = {};
+
+        HeapSort.sort(arr);
+
+        assertArrayEquals(expected, arr);
     }
 
     @Test
-    void printArray() {
+    public void testArrayWithDuplicates() {
+        int[] arr = {5, 4, 5, 5, 5};
+        int[] expected = {4, 5, 5, 5, 5};
+
+        HeapSort.sort(arr);
+
+        assertArrayEquals(expected, arr);
     }
 
     @Test
-    void main() {
+    public void testMassiveArray() {
+        int[] arr = new int[100000];
+        Random random = new Random();
+        for (int i = 0; i < 100000; i++)
+        {
+            int n = random.nextInt(Integer.MAX_VALUE);
+            arr[i] = n;
+        }
+
+        int[] expected = Arrays.copyOf(arr, 100000);
+
+        Arrays.sort(expected);
+
+        HeapSort.sort(arr);
+
+        assertArrayEquals(expected, arr);
     }
 }
