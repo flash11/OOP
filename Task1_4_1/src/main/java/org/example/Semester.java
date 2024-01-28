e7082b4c4068ef4301c2e8461a71224635a05971
Vladislav Dalevich <v.dalevich@g.nsu.ru>
2024-01-28 18:55:49 +0700

    update
    Signed-off-by: Vladislav Dalevich <v.dalevich@g.nsu.ru>

---
+++ Task1_4_1/src/main/java/org/example/Semester.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Semester {
+
+    private ArrayList<Subject> listSubjectsOfSemester;
+
+    /**
+     * getter
+     * @return listSubjectsOfSemester
+     */
+
+    public ArrayList<Subject> getListSubjectsOfSemester() {
+        return listSubjectsOfSemester;
+    }
+
+
+    /**
+     * constructor
+     */
+    public Semester() {
+        this.listSubjectsOfSemester = new ArrayList<Subject>();
+    }
+
+    /**
+     * method which add subjects and marks
+     * @param nameOfSubject
+     * @param mark
+     */
+    public void add(String nameOfSubject, int mark) {
+        var subj = new Subject(nameOfSubject, mark);
+        this.listSubjectsOfSemester.add(subj);
+    }
+
+
+    public boolean noThree(){
+        return listSubjectsOfSemester.stream().allMatch(subject -> subject.getMark() > 3 );
+    }
+
+    public boolean moneyBonus() {
+        return listSubjectsOfSemester.stream().allMatch(mark -> mark.getMark() == 5);
+    }
+
+
+}
