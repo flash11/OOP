fb1c141a02606d25116cc839d2d31a7defe60f01
flash11 <flash11@users.noreply.github.com>
2024-02-08 17:22:30 +0000

    Deploying to gh-pages from @ flash11/OOP@2536f82195bb23699b48501775cfdd7e60c439b6 🚀

--- Task_1_5_1/package-search-index.js
+++ Task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"exceptions"},{"l":"org.example"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"exceptions"},{"l":"operations"},{"l":"org.example"}]
