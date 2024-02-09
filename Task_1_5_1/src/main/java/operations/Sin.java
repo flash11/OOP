a4848947e7f7d43fad5016f86f3e4c2a14bd1719
flash11 <129732362+flash11@users.noreply.github.com>
2024-02-10 02:14:47 +0700

    Update Sin.java

--- Task_1_5_1/src/main/java/operations/Sin.java
+++ Task_1_5_1/src/main/java/operations/Sin.java
@@ -15,7 +15,7 @@ public class Sin extends Operation {
      * count.
      *
      * @return expression.
-     * @throws ArithmeticException.
+     * @throws ArithmeticException exception.
      */
     @Override
     public double count() throws ArithmeticException {
@@ -26,7 +26,7 @@ public class Sin extends Operation {
      * set from stack.
      *
      * @param stack.
-     * @throws IllegalArgumentFoundException.
+     * @throws IllegalArgumentFoundException exception.
      */
     @Override
     public void setArgsFromStack(Stack<Operation> stack) throws IllegalArgumentFoundException {
