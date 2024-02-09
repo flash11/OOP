fffc5981adcb06275177db6e4a9b496695f18c21
flash11 <129732362+flash11@users.noreply.github.com>
2024-02-10 03:24:16 +0700

    Update StudentBookTest.java

--- Task_1_4_1/src/test/java/org/example/StudentBookTest.java
+++ Task_1_4_1/src/test/java/org/example/StudentBookTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
- * Testing my methods
+ * Testing my methods.
  */
 public class StudentBookTest {
 
