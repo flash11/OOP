93b4cf1ffe033a173e3768b417753e5e17ccb404
flash11 <129732362+flash11@users.noreply.github.com>
2024-02-10 01:31:18 +0700

    Update Negative.java

--- Task_1_5_1/src/main/java/operations/Negative.java
+++ Task_1_5_1/src/main/java/operations/Negative.java
@@ -2,7 +2,6 @@ package operations;
 
 import exceptions.ArithmeticException;
 import exceptions.IllegalArgumentFoundException;
-
 import java.util.Stack;
 
 /**
@@ -16,7 +15,7 @@ public class Negative extends Operation {
      * count.
      *
      * @return expression.
-     * @throws ArithmeticException
+     * @throws ArithmeticException.
      */
     @Override
     public double count() throws ArithmeticException {
@@ -26,8 +25,8 @@ public class Negative extends Operation {
     /**
      * set from stack.
      *
-     * @param stack
-     * @throws IllegalArgumentFoundException
+     * @param stack.
+     * @throws IllegalArgumentFoundException.
      */
     @Override
     public void setArgsFromStack(Stack<Operation> stack) throws IllegalArgumentFoundException {
@@ -35,8 +34,5 @@ public class Negative extends Operation {
             throw new IllegalArgumentFoundException("");
         }
         expression = stack.pop();
-
-
-
     }
 }
