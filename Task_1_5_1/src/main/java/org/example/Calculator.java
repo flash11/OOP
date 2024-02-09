bc153856f71377cc3948d1230efeb1f243be708e
flash11 <129732362+flash11@users.noreply.github.com>
2024-02-10 01:10:34 +0700

    Update Calculator.java

--- Task_1_5_1/src/main/java/org/example/Calculator.java
+++ Task_1_5_1/src/main/java/org/example/Calculator.java
@@ -1,14 +1,15 @@
 package org.example;
 
-import exceptions.IllegalArgumentFoundException;
-import exceptions.LogarithmIncorrectArgumentException;
-import exceptions.IllegalOperatorException;
-import exceptions.DivisionByZeroException;
-import exceptions.SquareRootLessThanZeroException;
 import exceptions.ArithmeticException;
+import exceptions.DivisionByZeroException;
+import exceptions.IllegalArgumentFoundException;
+import exceptions.IllegalOperatorException;
+import exceptions.LogarithmIncorrectArgumentException;
+import exceptions.SquareRootLessThanZeroException;
+import java.util.Scanner;
 import operations.Operation;
 
-import java.util.Scanner;
+
 
 
 /**
@@ -20,10 +21,10 @@ public class Calculator {
      * method where scan input data and calculate.
      */
     public static void calculator() {
-    /**
-    * using multi-catch that present all exceptions.
-     */
-        while(true) {
+        /**
+         * using multi-catch that present all exceptions.
+         */
+        while (true) {
 
             Scanner line = new Scanner(System.in);
             String expression = line.nextLine();
@@ -32,8 +33,9 @@ public class Calculator {
                 Double result = calculateExpression(expression);
                 System.out.println(result);
 
-            } catch (ArithmeticException | IllegalArgumentFoundException |
-                     IllegalOperatorException e) {
+            } catch (ArithmeticException 
+                     | IllegalArgumentFoundException 
+                     | IllegalOperatorException e) {
 
                 System.out.println(e.getMessage());
 
@@ -43,12 +45,13 @@ public class Calculator {
 
     /**
      * main logic of calculator.
+     *
      * @param expression input string
      * @return double expression.
      * @throws DivisionByZeroException Division By Zero
      * @throws IllegalArgumentFoundException Illegal Argument Found
-     * @throws LogarithmIncorrectArgumentException Logarithm Incorrect Argument
-     * @throws SquareRootLessThanZeroException Square Root Less Than Zero
+     * @throws LogarithmIncorrectArgumentException LogarithmIncorrectArgument
+     * @throws SquareRootLessThanZeroException SquareRootLessThanZero
      * @throws IllegalOperatorException Illegal Operator
      */
     public static Double calculateExpression(String expression) throws IllegalArgumentFoundException,
