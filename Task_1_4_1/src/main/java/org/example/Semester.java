fecd7bba8e189464802e25b0d4f62990287237ad
flash11 <129732362+flash11@users.noreply.github.com>
2024-02-10 03:28:54 +0700

    Update Semester.java

--- Task_1_4_1/src/main/java/org/example/Semester.java
+++ Task_1_4_1/src/main/java/org/example/Semester.java
@@ -11,7 +11,6 @@ public class Semester {
      * 
      * @return listSubjectsOfSemester result.
      */
-
     public ArrayList<Subject> getListSubjectsOfSemester() {
         return listSubjectsOfSemester;
     }
