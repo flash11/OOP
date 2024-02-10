be088b94a44cee13088901039ce842c230ee49e3
Vladislav Dalevich <v.dalevich@g.nsu.ru>
2024-02-10 23:34:28 +0700

    tmp
    Signed-off-by: Vladislav Dalevich <v.dalevich@g.nsu.ru>

--- Task_1_1_2/src/main/java/org/example/Polynomial.java
+++ Task_1_1_2/src/main/java/org/example/Polynomial.java
@@ -4,29 +4,40 @@ import java.util.Arrays;
 import java.util.Collection;
 
 /**
- * [1,2,3] translate 3x^3 + 2x^2 + 1
+ * [1,2,3] translate 3x^2 + 2x + 1
+ * [1, 2, 0, 3] translate 3x^3 + 2x + 1
  */
+
+// степени это индексы коэффициентов, причем по нулевому индексу следует слагаемое x^0
 public class Polynomial {
+
+    public static void main(String[] args) {
+        Polynomial p = new Polynomial(new double[]{1, -2, 3});
+        System.out.println(p);
+    }
     private double[] coefficients;
 
+// два констуктора
     public Polynomial() {
-        coefficients = new double[]{1};
+        this.coefficients = new double[]{0};
     }
 
-    public Polynomial(double x) {
-        coefficients = new double[]{x};
-    }
 
     public Polynomial(double[] coefficients) {
         this.coefficients = normalize(coefficients);
     }
 
+    // возвращает скопированный массив нужной длины
     public double[] copyCoefficients() {
         return Arrays.copyOf(coefficients, coefficients.length);
     }
 
+    // создаем новый сложенный полином
+
     public Polynomial plus(Polynomial anotherPolynomial) {
-        Polynomial largerPolynomial = this, smallerPolynomial = anotherPolynomial;
+        Polynomial largerPolynomial = this;
+        Polynomial smallerPolynomial = anotherPolynomial;
+
         if (this.coefficients.length < anotherPolynomial.coefficients.length) {
             largerPolynomial = anotherPolynomial;
             smallerPolynomial = this;
@@ -38,24 +49,11 @@ public class Polynomial {
         return new Polynomial(newCoefficients);
     }
 
-    public Polynomial differentiate(int times) {
-        if (times < 0) {
-            throw new IllegalArgumentException("negative differentiation degree");
-        }
-        if (times >= coefficients.length) {
-            return new Polynomial(new double[]{0});
-        }
-        double[] newCoefficients = new double[coefficients.length - times];
 
-        for (int i = 0; i < newCoefficients.length; i++) {
-            newCoefficients[i] = coefficients[i + times] * getDifferentiateCoefficient(i + times, times);
-        }
-        return new Polynomial(newCoefficients);
-    }
-
+// подсчет в точке
     public double evaluate(double arg) {
         double result = 0;
-        double x = 1;
+        double x = 1; // отражает степени x
         for (int i = 0; i < coefficients.length; i++) {
             result += coefficients[i] * x;
             x *= arg;
@@ -63,24 +61,13 @@ public class Polynomial {
         return result;
     }
 
-    public Polynomial times(Polynomial anotherPolynomial) {
-        double[] anotherCoefficients = anotherPolynomial.coefficients;
-        int newLength = this.coefficients.length + anotherPolynomial.coefficients.length - 1;
-        double[] newCoefficients = new double[newLength];
-
-        for (int i = 0; i < anotherCoefficients.length; i++) {
-            for (int j = 0; j < coefficients.length; j++) {
-                newCoefficients[i + j] += coefficients[j] * anotherCoefficients[i];
-            }
-        }
-        return new Polynomial(newCoefficients);
-
-    }
 
     public Polynomial minus(Polynomial anotherPolynomial) {
+
         return this.plus(anotherPolynomial.multiply(-1));
     }
 
+    // нужно для минуса
     public Polynomial multiply(double scalar) {
         double[] newCoefficients = this.copyCoefficients();
         for (int i = 0; i < newCoefficients.length; i++) {
@@ -89,27 +76,18 @@ public class Polynomial {
         return new Polynomial(newCoefficients);
     }
 
-
+    // дает максимальную степень, избавляемся от старших нулей
     private static double[] normalize(double[] coefficients) {
         int maximalDegree = coefficients.length - 1;
-        for (int i = coefficients.length - 1; i > 0 && coefficients[maximalDegree] == 0.; i--) {
-            if (coefficients[i] == 0) {
-                maximalDegree = i - 1;
-                break;
-            }
+
+        while (maximalDegree > 0 && coefficients[maximalDegree] == 0) {
+            maximalDegree --;
         }
         return Arrays.copyOf(coefficients, maximalDegree + 1);
     }
 
 
-    private static double getDifferentiateCoefficient(int powerDegree, int times) {
-        double res = 1;
-        for (int i = 0; i < times; i++) {
-            res *= powerDegree;
-            powerDegree--;
-        }
-        return res;
-    }
+    // сравнение двух полиномов
 
     @Override
     public boolean equals(Object obj) {
@@ -122,12 +100,17 @@ public class Polynomial {
         return false;
     }
 
+    // РАСПЕЧАТЫВАНИЕ ПОЛИНОМА
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
+
         int last = coefficients.length - 1;
+
         stringBuilder.append(getMemberRepresentation(coefficients[last], last));
+
         last--;
+
         for (; last >= 0; last--) {
             if (coefficients[last] == 0) {
                 continue;
@@ -154,7 +137,4 @@ public class Polynomial {
         return String.format("%sx%s", coefPart, powerDegreePart);
     }
 
-    public static Polynomial of(double... args) {
-        return new Polynomial(args);
-    }
 }
