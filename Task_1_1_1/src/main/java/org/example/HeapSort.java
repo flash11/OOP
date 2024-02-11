d1ef60222e944a112b1e669911796c7103c78b6c
flash11 <129732362+flash11@users.noreply.github.com>
2024-02-11 22:33:44 +0700

    Update HeapSort.java

--- Task_1_1_1/src/main/java/org/example/HeapSort.java
+++ Task_1_1_1/src/main/java/org/example/HeapSort.java
@@ -6,13 +6,14 @@ package org.example;
 public class HeapSort {
     /**
      * Sorting specify arr.
-     
+     *
      * @param arr array for sorting.
      */
-    static public void sort(int arr[]) {
+    public static void sort(int[] arr) {
         int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--)
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
+        }
         for (int i = n - 1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
@@ -21,14 +22,16 @@ public class HeapSort {
         }
     }
 
-    static private void heapify(int arr[], int n, int i) {
+    private static void heapify(int[] arr, int n, int i) {
         int max_elem = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
-        if (l < n && arr[l] > arr[max_elem])
+        if (l < n && arr[l] > arr[max_elem]) {
             max_elem = l;
-        if (r < n && arr[r] > arr[max_elem])
+        }
+        if (r < n && arr[r] > arr[max_elem]) {
             max_elem = r;
+        }
         if (max_elem != i) {
             int tmp = arr[i];
             arr[i] = arr[max_elem];
